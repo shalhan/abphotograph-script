7f4531e41c635c215a662b123e094bcc8ccd302d
shalhan <shalhanradifan44@gmail.com>
2019-02-17 22:52:07 +0700

    change query into urlQuery

--- index.php
+++ index.php
@@ -8,25 +8,24 @@
         <script>
             //check is url exist
             var r = new XMLHttpRequest();
-            r.open("GET", "http://localhost:8000/check?current_url="+window.location.href+'&token=68501fe8d004ef236c0370ce97eef8d1');
+            r.open("GET", "http://178.128.29.75:8000/check?current_url="+window.location.href+'&token=c10b35f0197494fbcbf57222faf45e61');
             r.onreadystatechange = function () {
             if (r.readyState != 4 || r.status != 200) return;
                 let res = JSON.parse(r.responseText)
                 if(res.code === 1)
-                    setIframe()
+                    setIframe(res.data)
             };
             r.send();
             //set iframe
-            function setIframe() {
+            function setIframe(data) {
                 //get query
                 const urlQuery = new URLSearchParams(window.location.search);
-                const query = urlQuery.get('couple');
                 //create iframe
                 var iframe = document.createElement('iframe');
                 //styling iframe
                 iframe.style.width = "100%";
                 iframe.style.height = "100vh";
-                iframe.src = 'http://localhost:8000?couple='+query+'&token=68501fe8d004ef236c0370ce97eef8d1';
+                iframe.src = 'http://178.128.29.75:8000?'+urlQuery+'&token=c10b35f0197494fbcbf57222faf45e61';
                 //styling body
                 document.body.style.margin = "0";
                 document.body.appendChild(iframe);
